d10839bb1ec99036b9aed26f5f77c9ef8b3f4f24
AlisonAO <alison_opeloyeru@hotmail.co.uk>
2017-10-04 21:19:24 +0100

    made edit and update forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,9 @@
 Rails.application.routes.draw do
-  get 'comments/index'
+  
+  resources :posts do 
+    resources :comments
+  end
 
-  get 'comments/show'
-
-  get 'comments/new'
-
-  get 'comments/create'
-
-  get 'comments/edit'
-
-  get 'comments/update'
-
-  get 'comments/destroy'
-
-  get 'posts/index'
-
-  get 'posts/show'
-
-  get 'posts/new'
-
-  get 'posts/create'
-
-  get 'posts/edit'
-
-  get 'posts/update'
-
-  get 'posts/destroy'
-
+  root "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
