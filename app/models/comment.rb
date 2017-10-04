c993557d4f199cceb8db07ed14aed34abcf4979f
AlisonAO <alison_opeloyeru@hotmail.co.uk>
2017-10-05 00:41:12 +0100

    added more data validations and edited button placement

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,12 @@ class Comment < ApplicationRecord
 	belongs_to :post
 
 	validates :username, :email, :location, :body, :rating, presence: true
+
+	validates :rating, :numericality => true
+
+	validates :username, uniqueness: true
+
+	validates :rating, :inclusion => { :in => 0..5, :message => " should be between 0 to 5" }
+
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,11 @@ class Post < ApplicationRecord
 	has_many :comments 
 
 	validates :title, :author, :theme, :image, :body, presence: true
+
+	validates :image, format: { with: URI.regexp }
+
+	validates :title, uniqueness: true
+
+	validates :body, length: { minimum: 100 }
+
 end
